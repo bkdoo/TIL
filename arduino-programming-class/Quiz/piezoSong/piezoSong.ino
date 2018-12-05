10093e540e5dcc3b7ab36c0ba6ef3a6a73ef918d
bkdoo <kyeongdoos@gmail.com>
2018-12-05 17:25:01 +0900

    3번째 버튼 BTS - 피땀눈물 추가

--- arduino-programming-class/Quiz/piezoSong/piezoSong.ino
+++ arduino-programming-class/Quiz/piezoSong/piezoSong.ino
@@ -1,14 +1,35 @@
-/*
-2개와 피에조 스피터 1개를 활용하여 
-버튼1을 눌렀을 때 동요 '학교종이 땡땡땡'의 앞소절을 
-버튼2를 눌렀을 때 뒷소절을 소리내 보자.
-*/
+#define C  262
+#define D  294
+#define E  330
+#define F  349
+#define G  392
+#define A  440
+#define B  494
+#define CC  523
+#define DD  587
+#define EE  659
+#define FF  698
+#define GG  784
+#define AA  880
+#define BB  988
+#define CCC 1047
+#define DDD 1175
+
+
+
+
+
+
 
 int front = 2;
 int back = 4;
+int bts = 7;
 
 
 int piezo = 8;
+
+
+
 
 void setup() {
   pinMode(front, INPUT);
@@ -24,84 +45,143 @@ void loop() {
     if (digitalRead(back)==HIGH){
       schoolbell_back(piezo);
     }
+
+    if (digitalRead(bts)==HIGH){
+      piddam(piezo);
+    }
   
 }
 
+void setSound(int piezo, int note, int meter) {
+  tone(piezo, note, meter);
+  delay(meter);
+  noTone(piezo);
+}
+
 void schoolbell_front(int piezo){
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 440, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 440, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 1000);
-  delay(1000);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 294, 1500);
-  delay(1500);
-  noTone(piezo);
+  setSound(piezo, G, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, A, 300);
+  setSound(piezo, A, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, E, 600);
+  setSound(piezo, G, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, E, 300);
+  setSound(piezo, E, 300);
+  setSound(piezo, D, 900);
   
 }
 
 void schoolbell_back(int piezo){
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 440, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 440, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 1000);
-  delay(1000);
-  noTone(piezo);
-  tone(piezo, 392, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 294, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 330, 500);
-  delay(500);
-  noTone(piezo);
-  tone(piezo, 262, 1500);
-  delay(1500);
-  noTone(piezo);
+  setSound(piezo, G, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, A, 300);
+  setSound(piezo, A, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, G, 300);
+  setSound(piezo, E, 600);
+  setSound(piezo, G, 300);
+  setSound(piezo, E, 300);
+  setSound(piezo, D, 300);
+  setSound(piezo, E, 300);
+  setSound(piezo, C, 900);
+}
+
+void piddam(int piezo){
+  setSound(piezo, D, 200);//내
+  setSound(piezo, D, 400);//피
+  setSound(piezo, A, 400);//땀
+  setSound(piezo, G, 400);//눈
+  setSound(piezo, A, 200);//물
+  setSound(piezo, A, 600);//물
+
+  delay(800);
+
+  setSound(piezo, D, 200);//내
+  setSound(piezo, D, 400);//마
+  setSound(piezo, D, 200);//지
+  setSound(piezo, A, 400);//막
+  setSound(piezo, G, 400);//춤
+  setSound(piezo, G, 200);//을
+  setSound(piezo, A, 200);//을
+  
+  setSound(piezo, G, 800);//다
+  delay(400);
+  setSound(piezo, F, 400);//가
+  setSound(piezo, F, 400);//져
+
+  setSound(piezo, DD, 200);//가~
+  setSound(piezo, CC, 300);
+  setSound(piezo, A, 200);
+  setSound(piezo, CC, 200);
+  delay(200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 400);
+  setSound(piezo, F, 400);
+  setSound(piezo, D, 400);
+  delay(200);
+
+  
+
+  setSound(piezo, DD, 200);//가~
+  setSound(piezo, CC, 30);
+  setSound(piezo, A, 200);
+  setSound(piezo, CC, 200);
+  delay(200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 600);
+
+  setSound(piezo, D, 200);//내
+  setSound(piezo, D, 400);//피
+  setSound(piezo, A, 400);//땀
+  setSound(piezo, G, 400);//눈
+  setSound(piezo, A, 200);//물
+  setSound(piezo, A, 600);//물
+
+  delay(800);
+
+  setSound(piezo, D, 200);//내
+  setSound(piezo, D, 400);//마
+  setSound(piezo, D, 200);//지
+  setSound(piezo, A, 400);//막
+  setSound(piezo, G, 400);//춤
+  setSound(piezo, G, 200);//을
+  setSound(piezo, A, 200);//을
+  
+  setSound(piezo, G, 800);//다
+  delay(400);
+  setSound(piezo, F, 400);//가
+  setSound(piezo, F, 400);//져
+
+  setSound(piezo, DD, 200);//가~
+  setSound(piezo, CC, 300);
+  setSound(piezo, A, 200);
+  setSound(piezo, CC, 200);
+  delay(200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 400);
+  setSound(piezo, F, 400);
+  setSound(piezo, D, 400);
+  delay(200);
+
+  
+
+  setSound(piezo, DD, 200);//가~
+  setSound(piezo, CC, 300);
+  setSound(piezo, A, 200);
+  setSound(piezo, CC, 200);
+  delay(200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 200);
+  setSound(piezo, A, 200);
+  setSound(piezo, G, 600);
+
 }
