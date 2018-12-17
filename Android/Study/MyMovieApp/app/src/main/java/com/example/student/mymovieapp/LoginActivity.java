b074b3ba6f56d08589bf428f0ac9d7bd0f3ef4a5
bkdoo <kyeongdoos@gmail.com>
2018-12-17 17:43:55 +0900

    MyMovieApp 퍼미션체크 기능 추가

--- Android/Study/MyMovieApp/app/src/main/java/com/example/student/mymovieapp/LoginActivity.java
+++ Android/Study/MyMovieApp/app/src/main/java/com/example/student/mymovieapp/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.student.mymovieapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,10 +21,48 @@ public class LoginActivity extends AppCompatActivity {
     Button btn_login;
     SharedPreferences sharedPref;
 
+
+    final static String[] STR = {"파일 읽기", "파일 쓰기", "인터넷", "진동"};
+    final static String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.VIBRATE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        int[] permissionCheck = {
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE),
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.INTERNET),
+                ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.VIBRATE)
+        };
+
+
+        for (int i = 0; i < permissionCheck.length; i++) {
+
+            if (permissionCheck[i] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(),
+                        STR[i] + " 권한 있음", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        STR[i] + " 권한 없음", Toast.LENGTH_LONG).show();
+
+                ActivityCompat.requestPermissions(LoginActivity.this,
+                        new String[]{
+                                PERMISSIONS[i]
+                        }, i);
+            }
+        }
+
 
         edt_loginId = (EditText) findViewById(R.id.edt_loginId);
         edt_loginPwd = (EditText) findViewById(R.id.edt_loginPwd);
@@ -40,14 +83,14 @@ public class LoginActivity extends AppCompatActivity {
                 String pwd = edt_loginPwd.getText().toString();
                 if (authUser(id, pwd)) {
 
-                        SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putString("autoLogin", id);
-                        editor.commit();
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("autoLogin", id);
+                    editor.commit();
 
-                        Intent intent = new Intent(LoginActivity.this,
-                                MovieListActivity.class);
-                        startActivity(intent);
-                        finish();
+                    Intent intent = new Intent(LoginActivity.this,
+                            MovieListActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
@@ -79,5 +122,42 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 0:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), STR[0] + "권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), STR[0] + "권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 1:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), STR[1] + "권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), STR[1] + "권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), STR[2] + "권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), STR[2] + "권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 3:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), STR[3] + "권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), STR[3] + "권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 }
 
