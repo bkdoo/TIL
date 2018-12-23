a3ad9bfc644cb1595602a128f40bf8ad75047761
bkdoo <kyeongdoos@gmail.com>
2018-12-23 20:29:04 +0900

    블랙잭 게임 구현을 통한 객체지향 이해하기-3
    Dealer 객체 구현 및 Card와 CardDeck의 역할 재정리

--- JavaStudy/OOP-Blackjack/src/main/Card.java
+++ JavaStudy/OOP-Blackjack/src/main/Card.java
@@ -3,10 +3,12 @@ package main;
 public class Card {
 	private String pattern;
 	private String denomination;
+	private int point;
 
-	public Card(String pattern, String denomination) {
+	public Card(String pattern, int index) {
 		this.pattern = pattern;
-		this.denomination = denomination;
+		this.denomination = this.numberToDenomination(index);
+		this.point = this.numberToPoint(index);
 	}
 
 	public String getPattern() {
@@ -24,10 +26,38 @@ public class Card {
 	public void setDenomination(String denomiation) {
 		this.denomination = denomiation;
 	}
-	
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
 	@Override
 	public String toString() {
-		return "Card{" + "pattern = " + pattern + ", denomiation = " + denomination +"}";
+		return "Card{" + "pattern = " + pattern + ", denomiation = " + denomination + "}";
+	}
+
+	private String numberToDenomination(int number) {
+		if (number == 1) {
+			return "A";
+		} else if (number == 11) {
+			return "J";
+		} else if (number == 12) {
+			return "Q";
+		} else if (number == 13) {
+			return "K";
+		}
+		return String.valueOf(number);
+	}
+
+	private int numberToPoint(int number) {
+		if (number >= 11) {
+			return 10;
+		}
+		return number;
 	}
 
 }
--- JavaStudy/OOP-Blackjack/src/main/Dealer.java
+++ JavaStudy/OOP-Blackjack/src/main/Dealer.java
@@ -1,16 +1,52 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dealer {
 
 	private List<Card> cards;
+	private static final int CAN_RECEIVE_POINT = 16;
 	
+	public Dealer() {
+		cards = new ArrayList<>();
+	}
+
 	public void receiveCard(Card card) {
-		
+		if(canReceiveCard()) {
+			this.cards.add(card);
+			this.showCards();
+		} else {
+			System.out.println("카드의 합이 17이상입니다. 더이상 카드를 받을 수 없습니다.");
+		}
 	}
 	
-	public List<Card> openCards(){
-		return null;
+	private void showCards() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("현재 보유 카드 목록 \n");
+		
+		for (Card card : cards) {
+			sb.append(card.toString());
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private boolean canReceiveCard() {
+		return getPointSum() <= CAN_RECEIVE_POINT;
+	}
+	
+	private int getPointSum() {
+		int sum = 0;
+		for (Card card : cards) {
+			sum += card.getPoint();
+		}
+		
+		return sum;
+	}
+
+
+	public List<Card> openCards() {
+		return this.cards;
 	}
 }
