1296c680b832654b2da99c711298ebf3c1d7ffff
bkdoo <kyeongdoos@gmail.com>
2018-12-23 16:31:50 +0900

    블랙잭 게임 구현을 통한 객체지향 이해하기-2
    Gamer 객체 구현

--- JavaStudy/OOP-Blackjack/src/main/Gamer.java
+++ JavaStudy/OOP-Blackjack/src/main/Gamer.java
@@ -1,15 +1,36 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Gamer {
 	private List<Card> cards;
 
-	public void receiveCard(Card card) {
+	
+	public Gamer() {
+		cards = new ArrayList<>();
+	}
 
+	public void receiveCard(Card card) {
+		this.cards.add(card);
+		this.showCard();
+	}
+	
+	public void showCard() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("현재 보유 카드 목록 \n");
+		
+		for (Card card : cards) {
+			sb.append(card.toString());
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+		
 	}
 
 	public List<Card> openCards() {
-		return null;
+		return this.cards;
 	}
+	
+	
 }
